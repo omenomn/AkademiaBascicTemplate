15fc3d452c582dac952aa126f13d06e67d824a43
Daniel <daniel@omega323.com>
2016-11-03 14:47:17 +0100

    correction column type seeder

--- src/database/seeds/BasicTemplateSeeder.php
+++ src/database/seeds/BasicTemplateSeeder.php
@@ -51,7 +51,7 @@ class BasicTemplateSeeder extends Seeder
             'description' => '2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line ',
             'fb_id' => '3432432',
             'og_url' => 'http://gdata-akademia.dev/',
-            'og_tyoe' => 'article',
+            'og_type' => 'article',
             'og_title' => 'GDATA Akademia',
             'og_description' => '2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line ',
             'og_image' => 'og_default.png',
