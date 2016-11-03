c6ac37a549979d1b95ced932a51fde4073caa2a4
Daniel <daniel@omega323.com>
2016-11-03 14:52:22 +0100

    rename column

--- src/database/seeds/BasicTemplateSeeder.php
+++ src/database/seeds/BasicTemplateSeeder.php
@@ -55,7 +55,7 @@ class BasicTemplateSeeder extends Seeder
             'og_title' => 'GDATA Akademia',
             'og_description' => '2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line ',
             'og_image' => 'og_default.png',
-            'bottonm_content_slug' => 'default',
+            'bottom_content_slug' => 'default',
         ]);
 
         $styles = Style::get();
